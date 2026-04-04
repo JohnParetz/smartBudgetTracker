d2d7750affaaf8d505fc7fbe4322b833b1d6609c
John Paretzoglou <jmparet@pointpark.edu>
2026-04-04 00:39:32 -0400

    make app main page look better

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 let income = 0;
 let expenses = 0;
+let selectedType = "income";
+
+function setType(type) {
+  selectedType = type;
+
+  const incomeTab = document.getElementById("incomeTab");
+  const expenseTab = document.getElementById("expenseTab");
+
+  incomeTab.classList.remove("active");
+  expenseTab.classList.remove("active");
+
+  if (type === "income") {
+    incomeTab.classList.add("active");
+  } else {
+    expenseTab.classList.add("active");
+  }
+}
 
 function addEntry() {
   const amount = Number(document.getElementById("amount").value);
-  const type = document.getElementById("type").value;
 
   if (!amount) return;
 
-  if (type === "income") {
+  if (selectedType === "income") {
     income += amount;
   } else {
     expenses += amount;
